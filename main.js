a6269be5895e15271e7c445a65e4c2bc6362fa6f
Jenn Graves <jg.iamcontent@gmail.com>
2022-04-11 08:20:07 -0700

    Adds functional 'complete' and 'edit' buttons

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
-const storage = window.localStorage;
+var storage;
+var taskArray;
 
-const getTasks = () => {
+window.addEventListener('load', () => {
+  storage = window.localStorage;
+  taskArray = getTasks();
+
+  reloadTasks();
+  focusInput();
+})
+
+function getTasks() {
   let tasks = storage.getItem('tasks-data');
   array = tasks ? JSON.parse(tasks) : [];
   return array;
-}
-var taskArray = getTasks();
+} 
 
-// Helper functions for debugging
-function logTasks() {
-  console.log(JSON.parse(storage.getItem('tasks-data')));
+const setTasks = (taskArray) => {
+  storage.setItem('tasks-data', JSON.stringify(taskArray));
 }
+
+// initialize task array
 
 // Bind elements to JS variables
 const taskDisplay = document.getElementById('task-list');
 const submitButton = document.getElementById('submit-task');
-const resetButton = document.querySelector('#reset');
-const deleteButton = document.getElementById('delete');
-const editButton = document.getElementById('edit');
-// const completeTask = document.getElementById('item-text');
-const completeTask = document.getElementsByClassName('item-text');
+const resetButton = document.getElementById('reset');
 
 /* 
 *  Binding Event Handlers
@@ -44,18 +49,6 @@ resetButton.addEventListener('click', (event) => {
   resetList();
 })
 
-// // Complete item handler
-// if (completeTask) {
-//   for (var elem in completeTask) {
-//     console.log(elem);
-//   }
-//   // completeTask.addEventListener('click', (event) => {
-//   //   console.log('clicked')
-//   //   let item = event.target;
-//   //   toggleComplete(item);
-//   // })
-// }
-
 const focusInput = () => {
   document.getElementById('task-input').focus();
 }
@@ -63,16 +56,35 @@ const focusInput = () => {
 // Task action handlers
 const actionEventListener = (event) => {
   event.preventDefault();
+
+  // EDIT
   if (event.target.value === "Edit") {
-    console.log('Edit');
-  } else if (event.target.value === "Delete") {
+
+    // Hide action buttons
+    event.target.parentNode.style.display = 'none';
+
+    // Access text in ancestor div
+    let itemToEdit = event.target.parentNode.parentNode.firstChild;
+
+    editItem(itemToEdit);
+  } else {
     var element = event.target.parentNode;
 
-    while (element.getAttribute('class') !== 'task-list-item') {
-      element = element.parentNode;
-    }
 
-    deleteItem(element);
+    // COMPLETE
+    if (event.target.value === "Complete") {
+      completeTask(element.parentNode);
+
+      // DELETE
+    } else if (event.target.value === "Delete") {
+
+      // Find element to delete (task container) in lineage
+      while (element.getAttribute('class') !== 'task-list-item') {
+        element = element.parentNode;
+      }
+
+      deleteItem(element);
+    }
   }
 }
   
@@ -95,14 +107,20 @@ const setAttributes = (element, attributes) => {
  * Events | Methods
 */
 
-const submitTask = (event) => {
+
+const submitTask = () => {
   const input = document.getElementById('task-input');
+  taskArray = getTasks();
   let trimmedInput = input.value.trim();
-/* Note: Maybe let's use a regular expression here instead of checking for an empty string? */
+  /* Note: Maybe let's use a regular expression here instead of checking for an empty string? */
   if (trimmedInput === '') {
     window.alert("Please enter a task.");
     return;
-  } else {
+  } else if (taskArray.includes(trimmedInput)) {
+    window.alert("That task is already on your list.")
+    return;
+  } 
+  else {
     taskArray.push(trimmedInput);
     storage.setItem('tasks-data', JSON.stringify(taskArray));
     input.value = '';
@@ -111,23 +129,99 @@ const submitTask = (event) => {
   reloadTasks();
 }
 
-// const editItem = (item) => {
-//   return;
-// }
+const submitEdit = (input, index) => {
+  taskArray = getTasks();
+
+  if (input === '' || input.trim() === '') {
+    window.alert('why no task?');
+  }
+  let trimmedInput = input.trim();
+  
+  taskArray[index] = trimmedInput;
+  storage.setItem('tasks-data', JSON.stringify(taskArray));
+  reloadTasks();
+}
+
+const editItem = (item) => {
+  taskArray = getTasks();
+  var text = item.innerText;
+
+  var elemDiv = document.createElement('div');
+  elemDiv.setAttribute('id', 'edit-task-div');
+
+  var elem = document.createElement('input');
+  setAttributes(elem, {
+    type: 'text',
+    value: text,
+    id: 'edit-task',
+  })
+
+  var doneBtn = document.createElement('input');
+  setAttributes(doneBtn, {
+    type: 'button',
+    value: 'Done Editing',
+    id: 'done'
+  })
+
+  doneBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    submitEdit(event.target.previousSibling.value, taskArray.indexOf(text));
+  })
+
+  elem.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      submitEdit(event.target.value, taskArray.indexOf(text));
+    }
+  });
+
+  elemDiv.appendChild(elem);
+  elemDiv.appendChild(doneBtn);
+
+  item.parentNode.replaceChild(elemDiv, item);
+
+  document.getElementById('edit-task').focus();
+
+  return elemDiv;
+}
 
 const deleteItem = (item) => {
+  var taskText;
+  var itemIndex;
+  taskArray = getTasks();
 
+
+  // Bail if no tasks in storage
   if (!taskArray || taskArray.length === 0) {
     console.log('There are no tasks to delete')
     return;
   }
 
-  const taskText = item.innerText.trim();
-  const itemIndex = taskArray.indexOf(taskText);
+  // grab item text element
+  let itemTextEl = item.firstChild.firstChild;
+  if (itemTextEl.classList.contains('completed')) {
+    taskText = itemTextEl.innerText;
+    for (let i in taskArray) {
+      if (Array.isArray(taskArray[i])) {
+        if (taskArray[i][0] === taskText) {
+          itemIndex = i;
+          
+        }
+      }
+    }
+    // itemIndex = taskArray.indexOf(new Array([taskText, 'completed']));
+  } else {
+    taskText = item.innerText;
+    itemIndex = taskArray.indexOf(taskText);
+  }
+
+  // update storage
   const updatedTasks = taskArray.slice(0, itemIndex)
-    .concat(taskArray.slice(itemIndex + 1));
-  storage.setItem('tasks-data', JSON.stringify(updatedTasks));
-  item.remove();
+      .concat(taskArray.slice(itemIndex + 1));
+  setTasks(updatedTasks);
+
+  console.log("Deleted item \"" + taskText + "\" from storage.");
+
   reloadTasks();
 }
 
@@ -137,73 +231,126 @@ const resetList = () => {
   reloadTasks();
 }
 
-const toggleComplete = (item) => {
-  item.classList.toggle('completed');
+const completeTask = (item) => {
+
+  // Set up taskArray
+  taskArray = getTasks();
+
+  // Bail if no tasks in storage
+  if (!taskArray || taskArray.length == 0) {
+    console.log('Why no task array?');
+    return;
+  }
+
+  var taskDiv = item.firstChild;
+  var taskText = taskDiv.innerText;
+  var taskIndex = taskArray.indexOf(taskText);
+  var actionButtons = item.lastChild.childNodes;
+  actionButtons.forEach(button => {
+    button.setAttribute('hidden', true);
+  });
+  taskDiv.classList.toggle('completed');
+
+  // Set up new taskArray to update storage
+  var newTaskArray = taskArray.slice(0, taskIndex).concat(taskArray.slice(taskIndex + 1));
+  newTaskArray.push([taskText, 'completed']);
+  setTasks(newTaskArray);
+
+  // Refresh with new taskArray
+  reloadTasks();
 }
 
 const renderTasks = (taskArray) => {
-  if (document.getElementById('task-list').hasChildNodes()) {
+
+  // Setup : remove all tasks from the rendered list
+  if (document.getElementById('task-list').children) {
     var node = document.getElementById('task-list');
     while (node.firstChild) {
       node.removeChild(node.lastChild);
     }
   }
 
+  // Tell user there are no tasks to render
   if (!taskArray || taskArray.length === 0) {
+
     let element = document.createElement('h1');
     element.innerHTML = "No tasks to complete!";
     element.setAttribute('class', 'no-tasks');
     taskDisplay.appendChild(element);
+
   } else {
 
+    // Loop through taskArray to render items
     for (let i = 0; i < taskArray.length; i++) {
+      let task;
+      let completed;
+      
+      // Look for completed items in storage
+      if (typeof taskArray[i] === 'string') {
+        task = taskArray[i];
+      } else if (typeof taskArray[i] === 'object' && taskArray[i][1] === 'completed') {
+        task = taskArray[i][0];
+        completed = true;
+      } 
+
       var taskListItem = document.createElement('div');
       taskListItem.setAttribute('class', 'task-list-item');
       var itemContent = document.createElement('div');
       itemContent.setAttribute('class', 'item-content');
-      var itemText = document.createElement('div');
-      itemText.setAttribute('class', 'item-text');
-      itemText.innerText = taskArray[i];
       var actionButtons = document.createElement('div');
       actionButtons.setAttribute('class', 'action-buttons');
+      var itemText = document.createElement('div');
+      itemText.setAttribute('class', 'item-text');
+      itemText.innerText = task;
 
-      const editBtn = document.createElement('input');
-      setAttributes(editBtn, {
-        'type': 'button',
-        'id': 'edit',
-        'value': 'Edit'
-      })
+      actionButtons.addEventListener('click', actionEventListener);
+
 
       const deleteBtn = document.createElement('input');
       setAttributes(deleteBtn, {
-        'type': 'button',
-        'id': 'delete',
-        'value': 'Delete'
+        type: 'button',
+        id: 'delete',
+        value: 'Delete'
       })
 
-      actionButtons.addEventListener('click', actionEventListener);
-      actionButtons.appendChild(editBtn);
+      // Special treatment for completed items
+      // No 'edit' or 'complete' buttons; text crossed out
+      if (completed) {
+        itemText.classList.toggle('completed');
+
+        completed = false;
+      } else {
+
+        // Normal treatment for the remaining items
+        const editBtn = document.createElement('input');
+        setAttributes(editBtn, {
+          type: 'button',
+          id: 'edit',
+          value: 'Edit'
+        })
+
+        const completeBtn = document.createElement('input');
+        setAttributes(completeBtn, {
+          type: 'button',
+          id: 'complete',
+          value: 'Complete'
+        })
+
+        actionButtons.appendChild(completeBtn);
+        actionButtons.appendChild(editBtn);
+      }
+
       actionButtons.appendChild(deleteBtn);
       itemContent.appendChild(itemText);
       itemContent.appendChild(actionButtons);
       taskListItem.appendChild(itemContent);
       taskDisplay.appendChild(taskListItem);
-
-      // Complete item handler
-      itemText.addEventListener('click', (event) => {
-        let item = event.target;
-        toggleComplete(item);
-      })
+      
     }
   }
 }
 
 const reloadTasks = () => {
-  taskArray = getTasks();
-  renderTasks(taskArray);
+  renderTasks(getTasks());
 }
 
-window.addEventListener('load', () => {
-  reloadTasks();
-  focusInput();
-})
